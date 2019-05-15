8527bbbc59c2e07dcabbac9ead5ad52e407307d6
Km Ramirez <kmramirez.halcyondigital@gmail.com>
2019-05-15 15:36:13 +0800

    Documentations Added

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -37,8 +37,8 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+    {doc: 'coding-convention', label: 'Docs'},
+    {doc: 'plugins', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
